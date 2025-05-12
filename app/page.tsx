21219b9c744588a07ac6cdd0e19f4713bf3fbbf6
MARCOS MARTINS DA SILVA SANTOS <marcossan741@icloud.com>
2025-05-12 11:59:38 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "./_components/ui/button";
+
 export default function Home() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <Button>Hello world!</Button>
     </>
   );
 }
